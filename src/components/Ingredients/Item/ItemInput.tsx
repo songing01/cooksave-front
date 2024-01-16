b9a339de1875ad1771b210c9a678ce654eed175e
jimin song <songing01@gmail.com>
2024-01-17 00:44:27 +0900

    💄 식재료 직접 등록 화면 퍼블리싱

--- src/components/Ingredients/Item/ItemInput.tsx
+++ src/components/Ingredients/Item/ItemInput.tsx
@@ -1,7 +1,129 @@
-import React from "react";
+import icon1 from "@assets/ingredients/icon1.png";
+import styled from "styled-components";
+import IconList from "./IconList";
+import { FontBold, FontRegular } from "@style/font.style";
+import { useState } from "react";
 
 const ItemInput = () => {
-  return <div></div>;
+  const [isOpenList, setIsOpenList] = useState(false);
+  return (
+    <Div>
+      <img
+        className="icon"
+        src={icon1}
+        onClick={() => {
+          setIsOpenList(!isOpenList);
+        }}
+      />
+
+      <Detail>
+        <input className="name" placeholder="재료명을 입력하세요" />
+
+        <input className="price" placeholder="가격을 입력하세요" />
+      </Detail>
+
+      <CountWrapper>
+        <FontRegular size="20px" className="minus">
+          -
+        </FontRegular>
+        <Count>
+          <FontBold size="12px">1</FontBold>
+        </Count>
+        <FontRegular size="20px">+</FontRegular>
+      </CountWrapper>
+
+      {/* 리스트에서 아이콘 선택 */}
+      {isOpenList && (
+        <div className="icon-list">
+          <IconList
+            onClick={() => {
+              setIsOpenList(!isOpenList);
+            }}
+          />
+        </div>
+      )}
+    </Div>
+  );
 };
 
 export default ItemInput;
+
+const Div = styled.div`
+  position: relative;
+  width: 356px;
+  height: 81px;
+  border-radius: 6px;
+  background: #edf7ff;
+  box-sizing: border-box;
+  padding: 13px 9px 13px 13px;
+  display: flex;
+  gap: 8px;
+  align-items: center;
+
+  .icon {
+    width: 50px;
+  }
+
+  .icon-list {
+    position: absolute;
+    top: 10px;
+    left: 70px;
+    z-index: 1;
+  }
+`;
+
+const Detail = styled.div`
+  width: 216px;
+  height: 57px;
+
+  display: flex;
+  flex-direction: column;
+  align-items: start;
+  gap: 2px;
+
+  input {
+    border: none;
+    outline: none;
+    background-color: #edf7ff;
+  }
+
+  .price {
+    color: #7d7d7d;
+
+    font-family: Noto Sans KR;
+    font-size: 13px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+
+  .name {
+    font-family: Noto Sans KR;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 500;
+    line-height: normal;
+  }
+`;
+
+const Count = styled.div`
+  width: 30px;
+  height: 30px;
+  flex-shrink: 0;
+  border-radius: 40px;
+  background-color: #fff;
+
+  line-height: 27px;
+`;
+
+const CountWrapper = styled.div`
+  width: 58px;
+  display: flex;
+  gap: 5px;
+  justify-content: center;
+  align-items: center;
+
+  .minus {
+    margin-bottom: 5px;
+  }
+`;
--- src/pages/Create/SelfCreate.tsx
+++ src/pages/Create/SelfCreate.tsx
@@ -1,12 +1,69 @@
+import FitBtn from "@components/Buttons/FitBtn";
+import LongBtn from "@components/Buttons/LongBtn";
 import Header from "@components/Header/Header";
+import ItemInput from "@components/Ingredients/Item/ItemInput";
+import List from "@components/Ingredients/List/List";
+import { useState } from "react";
+import styled from "styled-components";
 
 //식재료를 직접 등록하는 페이지
 const SelfCreate = () => {
   return (
-    <div>
+    <Div>
       <Header isBack={true} title="식재료 등록" />
-    </div>
+
+      <div className="input">
+        <ItemInput />
+      </div>
+
+      <div className="btn">
+        <FitBtn text="추가하기" />
+      </div>
+
+      <div className="margin">
+        <List isEditing={true} />
+      </div>
+
+      <div className="bottom">
+        <LongBtn text="등록 완료" />
+      </div>
+    </Div>
   );
 };
 
 export default SelfCreate;
+
+const Div = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  .input {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  }
+
+  .btn {
+    width: 356px;
+    margin: 11px auto 25px;
+    display: flex;
+    flex-direction: column;
+    align-items: end;
+  }
+
+  .margin {
+    margin-bottom: 120px;
+  }
+
+  .bottom {
+    width: 100%;
+    position: fixed; //하단 고정
+    bottom: 0px;
+    padding: 30px;
+    display: flex;
+    justify-content: center;
+    background-color: #fff;
+  }
+`;
