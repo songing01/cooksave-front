684902420c27dd316d317270778b8edf689e5b49
jimin song <songing01@gmail.com>
2024-04-02 14:50:57 +0900

    ♻️ recoilPersist로 변경

--- src/services/store/ingredients.ts
+++ src/services/store/ingredients.ts
@@ -1,4 +1,10 @@
 import { atom } from "recoil";
+import { recoilPersist } from "recoil-persist";
+
+const { persistAtom } = recoilPersist({
+  key: "localStorage",
+  storage: localStorage,
+});
 
 //새롭게 등록 및 수정할 식재료 목록
 export const newListState = atom({
@@ -10,4 +16,5 @@ export const newListState = atom({
 export const myListState = atom({
   key: "myListState",
   default: [] as any,
+  effects_UNSTABLE: [persistAtom],
 });
