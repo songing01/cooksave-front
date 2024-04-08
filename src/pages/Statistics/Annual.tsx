704f046f02d0e98f1a6d333974779bdb687213ad
jimin song <songing01@gmail.com>
2024-04-08 17:11:06 +0900

    🐛 오타 수정

--- src/pages/Statistics/Annual.tsx
+++ src/pages/Statistics/Annual.tsx
@@ -1,10 +1,10 @@
 import GuideText from "@components/Common/GuideText";
 import Header from "@components/Header/Header";
 import AnnualCard from "@components/Statistics/AnnualCard";
-import React from "react";
+import React, { useEffect } from "react";
 import styled from "styled-components";
 
-const Anual = () => {
+const Annual = () => {
   return (
     <Div>
       <Header isBack={true} />
@@ -16,7 +16,7 @@ const Anual = () => {
   );
 };
 
-export default Anual;
+export default Annual;
 
 const Div = styled.div`
   display: flex;
